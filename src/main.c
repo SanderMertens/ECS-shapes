076f982b1bced204326bd4eb63ed382722ead34b
Sander Mertens <sander.mertens8@gmail.com>
2018-12-16 23:24:00 -0800

    Move DDS specific code to rti-connext-systems-shapes

--- src/main.c
+++ src/main.c
@@ -6,12 +6,8 @@
 #include <reflecs/systems/civetweb/civetweb.h>
 #include <reflecs/systems/admin/admin.h>
 #include <reflecs/util/time.h>
-#include "src/gen/ShapeType.h"
-#include "src/gen/ShapeTypeSupport.h"
+#include <ecs/systems/shapes/shapes.h>
 #include <unistd.h>
-
-#define DOMAIN_ID (0)
-#define TOPIC_NAME "Square"
 
 #define FPS (100.0)
 #define SHAPE_COUNT (8)
@@ -23,16 +19,7 @@
 
 /* -- Custom components -- */
 
-typedef float Size;
-
 typedef bool Collision[2];
-
-typedef struct DdsEntities {
-    DDS_DomainParticipant *dp;
-    DDS_Publisher *pub;
-    DDS_Topic *topic;
-    DDS_DataWriter *dw;
-} DdsEntities;
 
 /* -- System implementations -- */
 
@@ -141,6 +128,7 @@ void Create(EcsRows *rows) {
 /* -- System that initializes shapes -- */
 void InitShape(EcsRows *rows) {
     void *row;
+
     for (row = rows->first; row < rows->last; row = ecs_next(rows, row)) {
         EcsPosition2D *p = ecs_column(rows, row, 0);
         EcsVelocity2D *v = ecs_column(rows, row, 1);
@@ -160,72 +148,6 @@ void InitShape(EcsRows *rows) {
     }
 }
 
-/* -- System that synchronizes entities (shapes) to DDS -- */
-void DdsSync(EcsRows *rows) {
-    DdsEntities *w = ecs_column(rows, NULL, 0);
-    ShapeTypeExtendedDataWriter *dw = ShapeTypeExtendedDataWriter_narrow(w->dw);
-    char *colors[] = {"PURPLE", "BLUE", "RED", "GREEN", "YELLOW", "CYAN", "MAGENTA", "ORANGE"};
-
-    void *row;
-    for (row = rows->first; row < rows->last; row = ecs_next(rows, row)) {
-        EcsHandle entity = ecs_entity(row);
-        EcsPosition2D *p = ecs_column(rows, row, 1);
-        EcsRotation2D *r = ecs_column(rows, row, 2);
-        Size *size = ecs_column(rows, row, 3);
-
-        ShapeTypeExtended instance;
-        instance.parent.color = colors[entity % (sizeof(colors) / sizeof(char*))];
-        instance.parent.x = p->x;
-        instance.parent.y = p->y;
-        instance.parent.shapesize = *size;
-        instance.angle = r->angle;
-        instance.fillKind = 0;
-
-        ShapeTypeExtendedDataWriter_write(dw, &instance, &DDS_HANDLE_NIL);
-    }
-}
-
-/* -- DDS entity code -- */
-
-void DdsDeinit(EcsRows *rows) {
-    void *row;
-    for (row = rows->first; row < rows->last; row = ecs_next(rows, row)) {
-        DdsEntities *writer = ecs_column(rows, row, 0);
-        if (writer->dp)
-            DDS_DomainParticipant_delete_contained_entities(writer->dp);
-    }
-}
-
-void DdsInit(EcsRows *rows) {
-    void *row;
-    for (row = rows->first; row < rows->last; row = ecs_next(rows, row)) {
-        DdsEntities *writer = ecs_column(rows, row, 0);
-        writer->dp = DDS_DomainParticipantFactory_create_participant(
-            DDS_TheParticipantFactory, DOMAIN_ID, &DDS_PARTICIPANT_QOS_DEFAULT,
-            NULL, DDS_STATUS_MASK_NONE);
-        if (!writer->dp) break;
-
-        writer->pub = DDS_DomainParticipant_create_publisher(
-            writer->dp, &DDS_PUBLISHER_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
-        if (!writer->pub) break;
-
-        const char *type_name = ShapeTypeExtendedTypeSupport_get_type_name();
-        DDS_ReturnCode_t retcode = ShapeTypeExtendedTypeSupport_register_type(
-            writer->dp, type_name);
-        if (retcode != DDS_RETCODE_OK) break;
-
-        writer->topic = DDS_DomainParticipant_create_topic(
-            writer->dp, TOPIC_NAME, type_name, &DDS_TOPIC_QOS_DEFAULT, NULL,
-            DDS_STATUS_MASK_NONE);
-        if (writer->topic == NULL) break;
-
-        writer->dw = DDS_Publisher_create_datawriter(
-            writer->pub, writer->topic, &DDS_DATAWRITER_QOS_DEFAULT, NULL,
-            DDS_STATUS_MASK_NONE);
-        if (writer->dw == NULL) break;
-    }
-}
-
 /* -- Main -- */
 
 int main(int argc, char *argv[]) {
@@ -237,20 +159,16 @@ int main(int argc, char *argv[]) {
     ECS_IMPORT(world, EcsSystemsPhysics, ECS_2D);
     ECS_IMPORT(world, EcsSystemsCivetweb, 0);
     ECS_IMPORT(world, EcsSystemsAdmin, 0);
+    ECS_IMPORT(world, EcsSystemsShapes, 0);
 
     /* -- Init components and component families -- */
-    ECS_COMPONENT(world, Size);
     ECS_COMPONENT(world, Collision);
-    ECS_COMPONENT(world, DdsEntities);
     ECS_FAMILY(world, Shape, EcsPosition2D, EcsVelocity2D, EcsRotation2D, EcsAngularSpeed, Size, Collision);
 
     /* -- Init systems -- */
     ECS_SYSTEM(world, InitShape, EcsOnAdd,   EcsPosition2D, EcsVelocity2D, EcsRotation2D, EcsAngularSpeed, Size, Collision);
-    ECS_SYSTEM(world, DdsInit,   EcsOnAdd,   DdsEntities);
-    ECS_SYSTEM(world, DdsDeinit, EcsOnRemove, DdsEntities);
 
     /* -- Frame systems -- */
-    ECS_SYSTEM(world, DdsSync,   EcsOnFrame, SYSTEM.DdsEntities, EcsPosition2D, EcsRotation2D, Size);
     ECS_SYSTEM(world, Bounce,    EcsOnFrame, EcsPosition2D, EcsVelocity2D, Size, Collision);
     ECS_SYSTEM(world, Gravity,   EcsOnFrame, EcsVelocity2D);
     ECS_SYSTEM(world, Drag,      EcsOnFrame, EcsVelocity2D);
@@ -267,7 +185,7 @@ int main(int argc, char *argv[]) {
     ecs_enable(world, Drag_h, false);
     ecs_enable(world, Explode_h, false);
     ecs_enable(world, Create_h, false);
-    ecs_enable(world, DdsSync_h, true);
+    ecs_enable(world, EcsDdsSync_h, true);
 
     /* -- Start admin -- */
     ecs_set(world, 0, EcsAdmin, {.port = 9090});
